fb5f9b2b3aa26eeccc4c30fb7e077c39ebb91074
Andre Campos <andre.tatibano@gmail.com>
2022-02-11 08:31:02 -0300

    updae some data

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"runtime"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,7 +14,10 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		c.Response().Header.Add("Content-Type", "application/json")
-		return c.JSON("Testando uso de memoria heroku")
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+
+		return c.JSON("Testando uso de memoria heroku: " + bToMb(m.Alloc) + "Mb")
 	})
 
 	port := os.Getenv("PORT")
@@ -22,5 +27,8 @@ func main() {
 	}
 	log.Printf("Listening on port %s\n\n", port)
 	app.Listen(":" + port)
+}
 
+func bToMb(b uint64) string {
+	return strconv.FormatUint(b/1024/1024, 10)
 }
